80f246208d6d8946f67c4abb970f63f29d5390ca
Lamond Dixon <dixonlamond@gmail.com>
2015-04-07 16:32:55 -0600

    FlipACoin

---
+++ FlipACoin/FlipACoin/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipACoin
+{
+    class Program
+    {
+       static Random rng = new Random();
+       static int headsCounter = 0;
+        static int tailsCounter = 0;
+       
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i < 1000; i++);
+            }
+        
+        {
+        Console.WriteLine("#Heads: " + headsCounter);
+        Console.WriteLine("Tails: " + tailsCounter);
+        Console.Readkey();
+      }
+        public static string FlipACoin()
+       
+            if (rng.Next(2) == 0)
+            {
+                // Heads
+                headsCounter++;
+                return "Heads";
+            }
+            else
+            {
+                //Tails
+                tailsCounter++
+                return "Tails"
+            }
+        }
+    }
+}
